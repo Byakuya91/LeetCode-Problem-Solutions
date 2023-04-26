515d52d94369f55ef39f2172194067f477e51af6
Byakuya91 <nganti91@yahoo.com>
2023-04-26 11:26:04 -0400

    Time: 1518 ms (41.28%), Space: 30.5 MB (64.10%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
@@ -1,5 +1,6 @@
 class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
+        # NEW SOLUTION USING Dynamic programming 
         n = len(s)
         dp = [[0]*n for _ in range(n)]
         
@@ -13,7 +14,40 @@ class Solution:
         
         return dp[0][n-1]
 
+   # OLD SOLUTION TIME OUT LIMIT
+# class Solution:
+#     def longestPalindromeSubseq(self, s: str) -> int:
+#         cache = {}
+
+#         # We will approach this recursively
+#         # STEP ONE: create a dfs function with l and r pointers as parameters
+#         def dfs(l,r):
+#             # BASE case: 
+#             # when l or right is out of bounds
+#             if l < 0 or r == len(s):
+#                 return 0
+#             # if l and r are inside the cache
+#             if (l,r) in cache:
+#                 #  value inside the cache
+#                 return cache[(l,r)]
+            
+#             # if both characters in the pointers match each other
+#             if s[l] == s[r]:
                 
+#                 cache[(l,r)] = length = 1 if l == r else 2 + dfs(l+1,r-1)
+                
+#             else:
+#                 cache[(l,r)] =  max(dfs(l+1,r), dfs(l,r-1))
+                
+#             return cache[(l,r)]
+                
+#         # Loop through every position in the input string
+#         for i in range(len(s)):
+#             dfs(i,i)  # Odd length palindromic sub-sequences
+#             dfs(i,i+1)  # Even length palindromic sub-sequences
+            
+#         return max(cache.values())
+
             
             
         
