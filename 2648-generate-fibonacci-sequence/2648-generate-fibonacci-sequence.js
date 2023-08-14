be7c04778d299ffe4dd7fa30c4749216b6b733cb
Byakuya91 <nganti91@yahoo.com>
2023-08-14 11:48:17 -0400

    Time: 44 ms (95.33%), Space: 42.3 MB (23.89%) - LeetHub

--- 2648-generate-fibonacci-sequence/2648-generate-fibonacci-sequence.js
+++ 2648-generate-fibonacci-sequence/2648-generate-fibonacci-sequence.js
@@ -2,23 +2,37 @@
  * @return {Generator<number>}
  */
 var fibGenerator = function*() {
-//     create two variables to hold fibno numbers
+//     PLAN OF ATTACK
+//  UNDERSTAND THE PROBLEM(DONE)
+// WRITE OUT THE SOLUTION
+    
+// STEP ONE: create the terms n1, n2
     let n1 = 0, n2 = 1;
     
- while(true){
-//      Using a yield to return a value in a generator function
-      yield n1;
-     [n1,n2] = [n2, n1+n2];
-     
-//      OLD CODE
-    //  //  update the fibonnaci numbers with a temporary variable and n2 and n1
-    // let temp = n2;
-    // n2 = n1+n2
-    // // Note, the temp is storing the OLD n2 value 
-    // n1 = temp     
-     
- }
+//  ADDITIONAL STEP: while loop to traverse through list of numbers
+while(true){
+// //     OLD CODE
+//     yield n1;
+//     // STEP TWO: create a temp variable to store the numbers and update n2
+//     let temp = n2;
+//     n2 = n1+n2
     
+// //  STEP THREE: reassign n1 to equal the temp(temp holds the OLD n2 variable)
+    
+//     n1 = temp 
+    
+// NEW CODE: deconstructing
+    yield n1;
+    [n1,n2] = [n2, n1+n2]
+    
+    
+    
+    
+}
+    
+    
+// NOTE THE LACK OF RETURN STATEMENT BECAUSE OF THE GENERATOR FUNC
+
     
 };
 
