414716ce7fd669d1da992e1d808b708cbea69798
Byakuya91 <nganti91@yahoo.com>
2023-08-14 11:07:50 -0400

    Time: 72 ms (57.96%), Space: 16.2 MB (95.52%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,25 +1,12 @@
-class Solution(object):
-    def isPalindrome(self, x):
-        """
-        :type x: int
-        :rtype: bool
-        """
-        
-      #While loop potentially needs to be utilized 
-       # Base conditions to used if the number is negative 
-        if x<0:
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
             return False
-        # store the number inside of a variable to be able to modify it.
-        number = x
-        # store the reverse of the number
-        reverse_num = 0
         
-        # While loop to check the number
-        while number:
-            # Reverseing the number by dividing by 10 until it hits zero
-            # After the loop check if the output is greater than the range (−231, 231 − 1).
-            # At last, return the output with the correct sign (positive or negative).
-            reverse_num = reverse_num * 10 + number % 10
-            number //=10
-         # checking to see if reverse_num equals the original x value   
-        return x == reverse_num
+        if str(x) == str(x)[::-1]:
+            return True
+        
+        return False
+
+       
+        
