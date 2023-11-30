446147386a91a06454427b5da9073e2a6367e216
Neil <nganti91@yahoo.com>
2023-11-30 09:53:35 -0500

    Time: 64 ms (6.05%), Space: 41.4 MB (94.68%) - LeetHub

---
+++ 2235-add-two-integers/2235-add-two-integers.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} num1
+ * @param {number} num2
+ * @return {number}
+ */
+var sum = function(num1, num2) {
+//     STEP ONE: define variable and add numbers
+    let s_value = num1 + num2;
+// STEP TWO: return value
+    return s_value;
+    
+};
